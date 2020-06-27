24cf973b66686fc1cae677656db6b849bfc6d425
fbakly <fouadelbakly130@gmail.com>
2020-06-27 13:40:32 +0200

    Fixed spacing issue and added pacman updates

--- blocks.h
+++ blocks.h
@@ -1,18 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"MEMORY ", 	"echo $(free -m | grep -i mem | awk '{print $3}')/$(free -m | grep -i mem | awk '{print $2}') MB ", 	1, 	0},
-	{" 﨎", 	"expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000 ", 	1, 	0},
+	{"  ", 	"pacman -Qu", 			3600, 	0},
 
-	{" 直 ", 	"iwgetid -r ", 			60, 	0},
+	{"MEMORY ", "echo $(free -m | grep -i mem | awk '{print $3}')/$(free -m | grep -i mem | awk '{print $2}') MB", 	1, 	0},
 
-	{"  ", 	"cat /sys/class/power_supply/BAT*/capacity | tr '\n' ' ' ", 60, 0},
+	{"﨎", 	"expr $(cat /sys/class/thermal/thermal_zone0/temp) / 1000", 	1, 	0},
 
-	{" 墳 ", 	" pamixer --get-volume ", 0, 	10},
+	{"直 ", 	"iwgetid -r", 			60, 	0},
 
-	{"  ", 	"date +\"%H:%M\" ", 		30, 	0},
+	{" ", 	"cat /sys/class/power_supply/BAT*/capacity | tr '\n' ' '", 60, 0},
 
-	{"  ", 	"date +\"%d-%m-%Y\" ", 	3600, 	0},
+	{"墳 ", 	"pamixer --get-volume", 0, 	10},
+
+	{" ", 	"date +\"%H:%M| %d-%m-%Y\"", 		30, 	0},
+
+	//{"  ", 	"date +\"%d-%m-%Y\"", 	3600, 	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
